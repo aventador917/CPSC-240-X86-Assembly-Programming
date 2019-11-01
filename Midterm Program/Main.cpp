f37dc61074b0318a5b7632262b0d3907f6f86df2
Lappland <43505649+aventador917@users.noreply.github.com>
2019-10-31 19:54:12 -0700

    Update Main.cpp

--- Midterm Program/Main.cpp
+++ Midterm Program/Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 extern  "C" int Manage();
 int main()
 {
-                cout<< "This is the CS 240 midterm written by Paul Tang."<<endl;
+                cout<< "This is the CS 240 midterm written by Your Name."<<endl;
                 int number = Manage();
                 cout<< "The main program received this number: "<<number<<" and will now return to the operating system."<<endl;
 
