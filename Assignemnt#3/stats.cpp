84021408dffa811db2f4942e81e8d5d9bb30e668
Lappland <43505649+aventador917@users.noreply.github.com>
2019-10-31 19:42:45 -0700

    Update stats.cpp

--- Assignemnt#3/stats.cpp
+++ Assignemnt#3/stats.cpp
@@ -6,7 +6,7 @@ int main(){
   int return_code;
   return_code = StatisticalNum();
   cout<<"Welcome to Statistical Numbers";
-  cout<<"\nBought to you by fanghua gu";
+  cout<<"\nBought to you by Your Name";
   cout<<"This is the C++ program responding. Here we received the number "<<return_code<<"Have a nice day";
   cout<<"\nThe C++ program will not return a zero to the operating system.";
   return 0;
